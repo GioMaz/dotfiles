ee5af5063ea8120d2eb26d9cc315be134e4823e5
GioMaz <giovimaz2003@gmail.com>
2022-01-11 09:49:12 +0100

    powershell profile

---
+++ Profile.ps1
@@ -0,0 +1,15 @@
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Aliases
+Set-Alias vim nvim
+Set-Alias vi nvim
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
