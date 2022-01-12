9c9215fe7af3eb797d65fcb58e975059a6a16429
GioMaz <giovimaz2003@gmail.com>
2022-01-12 22:17:02 +0100

    zsh like completion

--- Profile.ps1
+++ Profile.ps1
@@ -2,6 +2,9 @@
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 # Aliases
 Set-Alias vim nvim
